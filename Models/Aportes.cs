716980296e3747079420e6158068cbed6646d0c2
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-02-17 19:54:55 -0400

    los modelos ya crados

---
+++ Models/Aportes.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Angel_Paredes_AP_AP1.Models
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+        [Display(Name = "Fecha")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten números")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name = "Persona")]
+        public required string Persona { get; set; }
+
+        [Display(Name = "Observación")]
+        public string? Observacion { get; set; }
+
+        [Required(ErrorMessage = "El monto es obligatorio")]
+        [Range(1, 200000.00, ErrorMessage = "Debe ser entre 1 y 200,000")]
+        [Display(Name = "Monto")]
+        public decimal Monto { get; set; }
+    }
+}
